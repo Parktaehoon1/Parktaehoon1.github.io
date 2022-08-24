3aea20d1a767f5808eaddc88ff81ef3b3f48a23b
Parktaehoon1 <92parkth@gmail.com>
2022-08-24 22:35:14 +0900

    구조잡는중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){    
+    setTimeout(function () {
+        gsap.to(".split span", {
+            duration: 1,
+            opacity: 1,
+            y: 0,
+            stagger: 0.1
+        })
+    }, 2000)
+    
+})
+
 window.onload = function(){
-  
+
 }
