73fe110d0cc0b1dbe935f3a70760661e8bf54948
Parktaehoon1 <92parkth@gmail.com>
2022-09-06 22:37:12 +0900

    AOS, Jquery 겹침 문제 수정중

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){    
+$(document).ready(function () {
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
     setTimeout(function () {
         gsap.to(".split span", {
             duration: 1,
@@ -42,7 +53,19 @@ $(document).ready(function(){
     });
 })
 
-window.onload = function(){
+window.onload = function () {
     AOS.init();
+
+    // document.querySelectorAll(".depth1 li a").forEach(li => {
+    //     li.addEventListener("click", e => {
+    //         e.preventDefault();
+    //         document.querySelector(li.getAttribute("href")).scrollIntoView({
+    //             behavior: "smooth"
+    //         })
+    //     })
+    // })
+    
+
+
 
 }
